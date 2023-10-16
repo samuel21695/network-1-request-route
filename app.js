c9d79a78777ff4b6473a9849f598b74751e39911
samuel21695 <samuel21695@gmail.com>
2023-10-16 09:36:39 +0900

    server listen

--- app.js
+++ app.js
@@ -8,4 +8,11 @@ const server = http.createServer((req, res) => {
     res.writeHead(500);
     return res.end('서버에 문제가 생겼습니다.');
   }
+
+  console.log("어떤 요청이 들어오는지 확인", "url -> ", req.url, "method ->", req.method);
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`cli 창에서 컨트롤 누른 후 옆에 포트 누르면 편리하게 확인 -> http://localhost:${PORT}/`);
 })

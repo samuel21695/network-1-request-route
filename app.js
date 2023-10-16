25b7ef28903cf9ed41154df5c740153c4123d0d0
samuel21695 <samuel21695@gmail.com>
2023-10-16 09:29:07 +0900

    server error log

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const fs - require('fs');
+
+const server = http.createServer((req, res) => {
+  
+  // 가독성을 위한 단수 함수 래핑
+  function.serverErrorlog() {
+    res.writeHead(500);
+    return res.end('서버에 문제가 생겼습니다.');
+  }
+})
